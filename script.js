c39f65d4ab78edc1b08a0f7241c960e4433625fc
GersonPaulo <igorpaulo64@gmail.com>
2024-07-21 23:31:03 +0100

    erro hamburguer replace to hamburger

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function toggleMenu( ) {
-    const menu = document.querySelector(".menu-links")
-    
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburguer-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+
 }
